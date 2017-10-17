3d9d8df083c796dd8aa0a6aa754d31cd2d0ea3ee
SAndrii <andriyshvorak@gmail.com>
2017-10-17 16:46:07 +0300

    add variables in callfile

--- call.py
+++ call.py
@@ -5,10 +5,12 @@ import sys
 from pycall import CallFile, Call, Application
 
 def call(number):
-    call = Call('SIP/{}'.format(number), callerid="Asterisk robot <4444>")
+    call = Call('SIP/{}'.format(number), callerid="Asterisk robot <4444>", wait_time=60, retry_time=60, max_retries=2, variables=vars)
     action = Application('Playback', 'hello-world')
-    c = CallFile(call, action, user='asterisk')
+    c = CallFile(call, action, user='asterisk', archive=True)
     c.spool()
+
+vars = {'FAXFILE': 'some.pdf', 'FAXHEADER': '', 'DESTINATION': '', 'EMAIL': 'andriyshvorak@gmail.com'}
 
 if __name__ == '__main__':
     call(sys.argv[1])
